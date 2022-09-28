e558555f7c169f2a7d57a622cccdebcc19dee244
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-28 20:35:25 +0000

    implemented movement of snake

--- client.js
+++ client.js
@@ -1,19 +1,21 @@
 const net = require("net");
 
 const connect = module.exports = () => {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541
-  });
+  return new Promise((resolve, reject) => {
+    const conn = net.createConnection({
+      host: "165.227.47.243",
+      port: 50541
+    });
 
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connection Successful.");
-    conn.write("Name: FOO");
-  });
+    conn.setEncoding("utf8");
+    conn.on("connect", () => {
+      console.log("Connection Successful.");
+      conn.write("Name: FOO");
+    });
 
-  conn.on("data", (data) => {
-    console.log("Server message:", data);
+    conn.on("data", (data) => {
+      console.log("Server message:", data);
+    });
+    resolve(conn);
   });
-  return conn;
 }
--- play.js
+++ play.js
@@ -1,5 +1,32 @@
-const net = require("net");
 const connect = require("./client");
+const readline = require("readline");
 
-console.log("Connecting...");
-connect();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const main = async () => {
+  console.log("Connecting...");
+  const conn = await connect();
+  return conn;
+};
+
+main().then((conn) => {
+  process.stdin.on("keypress", (str, key) => {
+    if(key.ctrl && key.name === "c") {
+      console.log("Game quit.");
+      conn.destroy();
+    }
+    if (key.name === "w") {
+      conn.write("Move: up");
+    } else if (key.name === "a") {
+      conn.write("Move: left");
+    } else if (key.name === "s") {
+      conn.write("Move: down");
+    } else if (key.name === "d") {
+      conn.write("Move: right");
+    }
+  });
+});
